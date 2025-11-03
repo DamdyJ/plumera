4e056ed7e0e52c7ca00bed0300d662f267fc8ae1
Damdy Junaedi <damdyjuns@gmail.com>
2025-11-03 23:16:02 +0700

    chore: add helmet middleware for security headers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express, { json, urlencoded } from 'express'
+import helmet from 'helmet';
 
 const app = express();
 app.use(urlencoded({ extended: true }))
 app.use(json())
+app.use(helmet())
 
 app.get("/api/test", (req, res) => {
     res.send("Hello World!");
