abdac1c918a6931130aeb3a5b463f8d7bf66c418
Damdy Junaedi <damdyjuns@gmail.com>
2025-11-13 14:21:09 +0700

    feat: add clerk integration

--- client/src/components/file-upload.tsx
+++ client/src/components/file-upload.tsx
@@ -4,12 +4,32 @@ import {
   DropzoneEmptyState,
 } from "@/components/ui/dropzone";
 import { useState } from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
 
 export default function FileUpload() {
+  const { getToken } = useAuth();
   const [files, setFiles] = useState<File[] | undefined>();
-  const handleDrop = (files: File[]) => {
-    console.log(files);
-    setFiles(files);
+
+  const handleDrop = async (files: File[]) => {
+    try {
+      const token = await getToken();
+      const formData = new FormData();
+      formData.append("file", files[0]);
+      const res = await axios.post(
+        "http://localhost:3000/api/chat",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      console.log(res.data);
+      setFiles(files);
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <Dropzone
