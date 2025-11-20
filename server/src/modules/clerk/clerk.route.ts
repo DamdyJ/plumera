958570bc4a904e5783efbd5f5d5116e5b6d794aa
Damdy Junaedi <damdyjuns@gmail.com>
2025-11-20 20:11:54 +0700

    refactor: remove unused Clerk webhook routes and user schema

--- server/src/modules/clerk/clerk.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { clerkWebhooks } from "./clerk.webhooks.controller";
-
-const router = express.Router();
-
-router.post("/", express.raw({ type: "application/json" }), clerkWebhooks);
-
-export { router as ClerkWebhookRouter };
