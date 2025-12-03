cbb8ab853fc54130064ca3bca089e100a82948a9
Damdy Junaedi <damdyjuns@gmail.com>
2025-12-03 10:09:33 +0700

    refactor: clean up imports and improve type annotations in HeroSection component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,6 @@ import { router } from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import { queryClient } from "./lib/queryClient";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
